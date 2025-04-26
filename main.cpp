47db4e7ef0f2c4d46cfa3456379fde6575c77625
liamgrundy <liamgrundy477@outlook.com>
2025-04-26 22:02:08 +0100

    configure 216MHz HSE

--- main.cpp
+++ main.cpp
@@ -6,6 +6,35 @@ extern "C" {
     uint8_t ucHeap[ configTOTAL_HEAP_SIZE ] __attribute__((aligned(8), section(".freertos_heap")));
 }
 
+void configure_clock() {
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY));
+
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    PWR->CR1 |= PWR_CR1_VOS;
+
+    RCC->PLLCFGR = (12 << RCC_PLLCFGR_PLLM_Pos) |
+                   (216 << RCC_PLLCFGR_PLLN_Pos) |
+                   (0 << RCC_PLLCFGR_PLLP_Pos) |
+                   (9 << RCC_PLLCFGR_PLLQ_Pos) |
+                   RCC_PLLCFGR_PLLSRC_HSE;
+
+    RCC->CR |= RCC_CR_PLLON;
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+
+    FLASH->ACR = FLASH_ACR_LATENCY_7WS |
+                 FLASH_ACR_PRFTEN |
+                 FLASH_ACR_ARTEN;
+
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+
+    RCC->CFGR &= ~RCC_CFGR_SW;
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+}
+
 void init_gpio(uint8_t pin) {
     // Set PB4 as general purpose output (mode 01)
     GPIOB->MODER &= ~(0x3 << (pin * 2));     // Clear mode bits
@@ -44,6 +73,8 @@ void flash_led(void *pParameters) {
 
 
 int main() {
+    configure_clock();
+
     // Enable clock to GPIOB (bit 1 in AHB1ENR)
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 
